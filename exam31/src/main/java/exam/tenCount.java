18ef3f358be0a9e5e47b87477c0ef3d05866f51d
nohsa97 <81568105+nohsa97@users.noreply.github.com>
2023-07-11 00:30:44 +0900

    first.

---
+++ exam31/src/main/java/exam/tenCount.java
@@ -0,0 +1,40 @@
+package exam;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class tenCount
+ */
+@WebServlet("/ten")
+public class tenCount extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public tenCount() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.println("<h1>1-10 출력</h1>");
+		for (int i = 1; i <= 10; i++) {
+			out.println(i+"<br>");
+		}
+		out.close();
+	}
+
+}
